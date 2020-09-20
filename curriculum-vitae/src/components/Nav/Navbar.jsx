8ec1e7024ce053828b630c9aaeded6db509ae319
Maltex <henrik.nilsson88@gmail.com>
2020-09-20 22:19:53 +0200

    hamburger complete

--- curriculum-vitae/src/components/Nav/Navbar.jsx
+++ curriculum-vitae/src/components/Nav/Navbar.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import styled from 'styled-components'
-import RightNav from './RightNav.jsx';
+import styled from 'styled-components';
 
+import Burger from './Burger';
 const Nav = styled.nav`
-    width: 100%;
-    height: 55px;
-    border-bottom: 2px solid black;
-    padding: 0 20px;
-    display: flex;
-    justify-content: space-between;
+  width: 100%;
+  height: 55px;
+  border-bottom: 2px solid black;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
 
-    .logo {
-        padding: 15px 0;
-    }
+  .logo {
+    padding: 15px 0;
+  }
 `;
 const Navbar = () => {
-    return (
-        <Nav>
-            <div className="logo">@ Henrik Nilsson</div>
-            <RightNav />
-        </Nav>
-    )
-}
+  return (
+    <Nav>
+      <div className="logo">@ Henrik Nilsson</div>
+      <Burger />
+    </Nav>
+  );
+};
 
-
-export default Navbar
+export default Navbar;
--- curriculum-vitae/src/components/Nav/RightNav.jsx
+++ curriculum-vitae/src/components/Nav/RightNav.jsx
@@ -1,58 +1,60 @@
 import React from 'react';
-import { NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Ul = styled.ul`
-    list-style: none;
-    display: flex;
-    flex-flow: row nowrap;
-    li {
-        padding: 18px 10px;
-    }
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
 
-    @media (max-width: 768px) {
-        flex-flow: column nowrap;
-        background-color: green;
-        position: fixed;
-        top: 0;
-        right: 0;
-        height: 100vh;
-        width: 300px;
-        padding-top: 3.5rem;
+  li {
+    padding: 18px 10px;
+  }
 
-      
-    }
-`; 
+  @media (max-width: 768px) {
+    transform: ${({ open }) => (open ? 'translateX(0)' : 'translate(100%)')};
+    transition: 0.3s ease-in-out;
+    flex-flow: column nowrap;
+    background-color: green;
+    position: fixed;
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+  }
+`;
 
-
-
-const RightNav = () => {
-    return (
-        <Ul>
-            <li>
-                <NavLink to="/" exact
-                style={{ textDecoration: 'none', color: 'white' }}
-                activeStyle={{
-                    color:'blue'
-                }}>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to="/projects"
-                style={{ textDecoration: 'none', color: 'white' }}
-                activeStyle={{
-                    
-                    color:'blue'
-                }}>Projects</NavLink>
-            </li>
-            <li>bajs</li>
-            <li>bajs</li>
-            <li>bajs</li>
-            <li>bajs</li>
-            <li>bajs</li>
-            <li>bajs</li>
-            <li>bajs</li>
-        </Ul>
-    )
-}
+const RightNav = ({ open, setOpen }) => {
+  return (
+    <Ul open={open}>
+      <li>
+        <NavLink
+          to="/"
+          exact
+          style={{ textDecoration: 'none', color: 'white' }}
+          activeStyle={{
+            color: 'blue'
+          }}
+          onClick={() => setOpen(!open)}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/projects"
+          style={{ textDecoration: 'none', color: 'white' }}
+          activeStyle={{
+            color: 'blue'
+          }}
+          onClick={() => setOpen(!open)}
+        >
+          Projects
+        </NavLink>
+      </li>
+    </Ul>
+  );
+};
 
 export default RightNav;
